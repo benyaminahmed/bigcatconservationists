6c997dff0873ba68d0ce3eb6c1b005b7b758b586
BenyaminAhmed <benyaminahmed2@gmail.com>
2018-03-03 14:13:18 +0000

    added valid member list + added a footer

--- js/join.js
+++ js/join.js
@@ -1,29 +1,65 @@
 const localStorageKey = "ba_members_names";
 
+const allowedNames = [
+    "ansh agarwal",
+    "leon kinsley",
+    "amos goodman",
+    "niam shah",
+    "anek shergill",
+    "vihaan vekaria",
+    "leo basset",
+    "aayan choudhry",
+    "yash patel",
+    "nayan patel",
+    "aaron scott",
+    "albert vasvari",
+    "blake blau",
+    "vivaan chhabra",
+    "aryan goria",
+    "aarav rajput",
+    "michael sawyer",
+    "zac waterman",
+    "sahib bisal",
+    "shay patel",
+    "ruben rockliffe",
+    "rohan shinn",
+    "sahib singh",
+    "daniel southon",
+    "oscar wilson",
+    "hamza shehabi",
+    "oliver ockleford"
+];
+
 function onSubmit() {
     const firstName = document.getElementById('firstName');
     const surname = document.getElementById('surname');
     const name = firstName.value + ' ' + surname.value;
-    addMember(name);
-    const memberNames = localStorage.getItem(localStorageKey)
-    if(memberNames) {
-        localStorage.setItem(localStorageKey, memberNames + ',' + name);
-    } else {
-        localStorage.setItem(localStorageKey, name);
+    if (name.trim().length > 0) {
+        addMember(name);
+        const memberNames = localStorage.getItem(localStorageKey)
+        if (memberNames) {
+            localStorage.setItem(localStorageKey, memberNames + ',' + name);
+        } else {
+            localStorage.setItem(localStorageKey, name);
+        }
     }
 }
 
-function addMember(name){
-    const list = document.getElementById('memberList');
-    const listItem = document.createElement('li');
-    const heading = document.createElement('h5');
-    heading.appendChild(document.createTextNode(name));
-    listItem.appendChild(heading);
-    list.appendChild(listItem);
+function addMember(name) {
+    name = name.toString().toLowerCase();
+
+    if (allowedNames.indexOf(name) !== -1) {
+        const list = document.getElementById('memberList');
+        const listItem = document.createElement('li');
+        const heading = document.createElement('h5');
+        heading.appendChild(document.createTextNode(name));
+        listItem.appendChild(heading);
+        list.appendChild(listItem);
+    }
 }
 
 const memberNames = localStorage.getItem(localStorageKey);
-if(memberNames) {
+if (memberNames) {
     const names = memberNames.split(',');
     names.forEach(name => {
         addMember(name);
